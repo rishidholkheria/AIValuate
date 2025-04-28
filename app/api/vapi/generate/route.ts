6b0fdf1ae9ecbb1a75035b9840590db2aa43d504
rishidholkheria <rishidholkheria2001@gmail.com>
2025-04-28 23:26:32 +0530

    logs..

--- app/api/vapi/generate/route.ts
+++ app/api/vapi/generate/route.ts
@@ -6,6 +6,7 @@ import { getRandomInterviewCover } from "@/lib/utils";
 
 export async function POST(request: Request) {
   const { type, role, level, techstack, amount, userid } = await request.json();
+  console.log(request.json(), userid)
 
   try {
     const { text: questions } = await generateText({
